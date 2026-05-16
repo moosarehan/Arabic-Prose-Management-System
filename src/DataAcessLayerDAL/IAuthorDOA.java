9de0a53f2722654e4a8d96b563644fb2ce1717a0
moosarehan <moosarehan4@gmail.com>
2026-05-17 01:50:54 +0500

    Add IAuthorDOA: Interface for Author data operations

---
+++ src/DataAcessLayerDAL/IAuthorDOA.java
@@ -0,0 +1,16 @@
+ package DataAcessLayerDAL;
+
+import java.util.ArrayList;
+
+import ModelDTO.Author;
+
+public interface IAuthorDOA {
+	   public boolean addAuthor(String name,String biography );
+	   public boolean updateAuthor(String oldname,String newname,String biography);
+	   public boolean deleteAuthor(String authorname);
+	   public Author retrieveAuthor (String keyword);
+	   public int searchAuthor(String authorname);
+	   
+	   public ArrayList<Author> retrieveAllAuthors(); 
+	   public String getAuthorByID(int id);
+}
